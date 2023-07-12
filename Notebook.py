11151538a261779ba8dc8724ce5788afb5692a54
Tessa Bogdanovich <surinastacey@gmail.com>
2023-07-12 14:12:13 +0300

    edit

--- Notebook.py
+++ Notebook.py
@@ -5,3 +5,10 @@ def add_note():
     text = input("Введите текст заметки: ")
     notebook[name] = text
     print(f"Заметка {name} успешно добавлена.")
+    
+def edit_note():
+    name = input("Введите название заметки, которую хотите отредактировать: ")
+    if name in notebook:
+       text = input("Введите новый текст: ")
+       notebook[name] = text
+    print(f"Текст {name} успешно изменен.")
